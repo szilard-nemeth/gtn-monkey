f98f3e0efcd9c7bd1a112e027bfc03bc560016da
Szilard Nemeth <szilard.nemeth88@gmail.com>
2019-12-20 10:53:51 +0100

    Load comments and parse GTN links

--- gtn-monkey.js
+++ gtn-monkey.js
@@ -41,11 +41,104 @@ function myHandler() {
     }
 $(document).on('iframeready', myHandler);
 
-function matchUrlsFromDescription() {
-	var description = $('#descriptionmodule p').text()
-	var urlRegex =/(\b(https?|ftp|file):\/\/[-a-zA-Z0-9+&@#\/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#\/%=~_|])/ig;
-	var found = description.match(urlRegex);
-	console.log(found)
+
+
+
+
+
+function waitForCommentsLoaded(funcToCall) {
+	var waitForEl = function(selector, callback, count) {
+	  if ($(selector).length == 0) {
+	    callback();
+	  } else {
+	    setTimeout(function() {
+	      if(!count) {
+	        count=0;
+	      }
+	      count++;
+	      console.log("count: " + count);
+	      if(count<10) {
+	        waitForEl(selector,callback,count);
+	      } else {return;}
+	    }, 1000);
+	  }
+	};
+
+	//<span class="show-more-comments" data-collapsed-count="18">Loading...</span>
+	var selector = $(".show-more-comments");
+
+	waitForEl(".show-more-comments", funcToCall);
 }
 
-loadGtnLinks(issueLinks.slice(0, 1))
+function findLinksInDescription() {
+	var description = $('#descriptionmodule p').html()
+	return findLinksInHtml(description)
+}
+
+function findLinksInComments() {
+	//Click on show more comments button
+
+	//<a class="collapsed-comments" href="/browse/CDH-76879?page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel&amp;showAll=true">
+	//<span class="collapsed-comments-line"></span>
+	//<span class="collapsed-comments-line"></span><span class="show-more-comments" data-collapsed-count="18">18 older comments</span></a>
+	$('.collapsed-comments').trigger('click')
+	
+
+	//Wait for comments to be loaded
+	//https://gist.github.com/chrisjhoughton/7890303
+	waitForCommentsLoaded(parseAndSaveComments)
+}
+
+function parseAndSaveComments() {
+	console.log("***COMMENTS LOADED");
+	//classes: twixi-wrap verbose actionContainer
+	var allLinks = []
+	$('.twixi-wrap > .action-body').each(function() {
+		// console.log($(this).html());
+		// console.log("found: " + findLinksInHtml($(this).html()))
+		var links = findLinksInHtml($(this).html())
+
+		if (links == null) {
+			window.localStorage.setItem(getLocalStorageKeyForJira(), JSON.stringify([]));
+		} else {
+			links = links.map(function(link) {
+			if (link.indexOf("gtn=") != -1) {
+				console.log("***found link matching GTN: " + link)
+				return link;
+			} else {
+				return null
+			}
+			})
+			//console.log("***links: " + JSON.stringify(links))
+			links = links.filter(function(link) {
+			  if (link == null) {
+			    return false;
+			  }
+			  return true;
+			})
+			if (links.length > 0) {
+				allLinks = allLinks.concat(links)
+			}
+		}
+ 	});
+
+	console.log("Found links for jira " + getJiraName() + ": " + JSON.stringify(allLinks))
+ 	window.localStorage.setItem(getLocalStorageKeyForJira(), JSON.stringify(allLinks));
+}
+
+function findLinksInHtml(html) {
+	var urlRegex =/(\b(https?|ftp|file):\/\/[-a-zA-Z0-9+&@#\/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#\/%=~_|])/ig;
+	var found = html.match(urlRegex);
+	return found
+}
+
+function getLocalStorageKeyForJira() {
+	return 'gtnmonkey_' + getJiraName()
+}
+
+function getJiraName() {
+	return window.location.href.split("browse/")[1]
+}
+ 
+findLinksInComments()
+// loadGtnLinks(issueLinks.slice(0, 1))
