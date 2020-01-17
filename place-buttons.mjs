eac3dda9df516c65df94f1ccda3a1c556639af11
Szilard Nemeth <szilard.nemeth88@gmail.com>
2020-01-17 09:35:30 +0100

    Fix place-buttons: Refer to gtnMonkey module functions differently: Use addEventListener

--- place-buttons.mjs
+++ place-buttons.mjs
@@ -18,10 +18,8 @@ function createButton(title, funcToCall, imgSrc, imgAlt, style) {
     }
 
     var id = title.toLowerCase().replace(/ /g, '-');
-    var href = `javascript:${funcToCall.name}();`
     var anchorClass = "aui-button aui-button-subtle issuenav-share no-icon"
-
-    var anchorDef = `<a id="${id}" class="${anchorClass}" href="${href}" title="${title}"
+    var anchorDef = `<a id="${id}" class="${anchorClass}" title="${title}"
                     ${style != undefined ? `style="${style}"` : `style=""`}>${title}</a>`
     var anchor = myjQuery(anchorDef.trim())
 
@@ -32,11 +30,14 @@ function createButton(title, funcToCall, imgSrc, imgAlt, style) {
 
     console.log("anchor:", anchor)
     anchor.appendTo(myjQuery('.saved-search-operations '));
+
+    //Add event listener when <a> is added to the DOM
+    document.querySelector('#' + id).addEventListener('click', funcToCall)
 }
 
 var filterPage = window.location.href.startsWith("https://jira.cloudera.com/issues/?filter=")
 if (filterPage) {
-    createButton("GTN Monkey!", GtnMonkey.findAllLinksFromJiraIssues, `${server_url}/monkey-icon.png`, "monkey-logo", "background-color: lightgreen")
+    createButton("GTN Monkey", GtnMonkey.findAllLinksFromJiraIssues, `${server_url}/monkey-icon.png`, "monkey-logo", "background-color: lightgreen")
     createButton("GTNM Show results", GtnMonkey.showResultsOverlay, `${server_url}/monkey-icon.png`, "monkey-logo")
     createButton("GTNM Clear results", GtnMonkey.cleanupStorage, `${server_url}/monkey-icon.png`, "monkey-logo", "background-color: lightcoral")
 }
